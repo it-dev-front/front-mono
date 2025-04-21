6cae5fb789f9dd611c66025577e9c0c76ecea47d
jungbyungwook <korjby400@gmail.com>
2025-04-22 01:40:37 +0900

    feat: banner

--- apps/fcdb/src/views/Ranking.tsx
+++ apps/fcdb/src/views/Ranking.tsx
@@ -8,9 +8,12 @@ export const Ranking = () => {
       <div className="flex flex-col min-h-screen pt-[60px]">
         <Header />
         <main className="flex-grow flex justify-center pb-[520px]">
-          <ol>
-            <li>ranking-page</li>
-          </ol>
+          <div>
+            <div className="h-40" />
+            <ol>
+              <li>ranking-page</li>
+            </ol>
+          </div>
         </main>
         <Footer />
       </div>
