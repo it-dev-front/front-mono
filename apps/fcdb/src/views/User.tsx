c4ca09fa0ed9b7fe75f577aa24bc2ff4c6818d36
jungbyungwook <korjby400@gmail.com>
2025-04-22 01:53:55 +0900

    chore: User view에 Footer 적용

--- apps/fcdb/src/views/User.tsx
+++ apps/fcdb/src/views/User.tsx
@@ -1,4 +1,6 @@
 import { Header } from "@/widgets/Header";
+import { Footer } from "@/widgets/Footer";
+
 import { UserInformation } from "@/widgets/UserInformation";
 
 interface UserPageProps {
@@ -9,11 +11,15 @@ export const User = ({ name }: UserPageProps) => {
   return (
     <div>
       <Header />
-      <div className="pt-4 flex justify-center">
-        <div>
-          <UserInformation />
-          <p className="pt-4">{name}</p>
-        </div>
+      <div className="flex flex-col min-h-screen pt-[60px]">
+        <main className="flex-grow flex justify-center pb-[520px]">
+          <div>
+            <div className="h-4" />
+            <UserInformation />
+            <p className="pt-4">{name}</p>
+          </div>
+        </main>
+        <Footer />
       </div>
     </div>
   );
