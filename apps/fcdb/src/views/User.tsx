dab8c25de979ea30f9f725cf3402de673a8548b8
dahoon06 <dahoon226@naver.com>
2025-04-23 23:08:58 +0900

    chore: 선수 프로필 이미지 변경

--- apps/fcdb/src/views/User.tsx
+++ apps/fcdb/src/views/User.tsx
@@ -10,8 +10,8 @@ export const User = ({ name }: UserPageProps) => {
   return (
     <div>
       <Header />
-      <div className="w-full min-w-[366px] flex flex-col min-h-screen pt-[60px]">
-        <main className="w-full flex-grow flex justify-center py-0 md:py-[1rem]">
+      <div className="w-full min-w-[366px] flex flex-col min-h-screen pt-[62px]">
+        <main className="w-full flex-grow flex justify-center py-0 md:py-[1rem] border-t border-[#424242] md:border-t-0">
           <ProfileSummary />
         </main>
         <Footer />
