ffc5b15f8b2ceff934b7a414e71eacf4c9b4a4a8
JihoonPark <z1@instiz.net>
2025-04-27 23:21:42 +0900

    feat: possession indicator 구현 #7

---
+++ apps/fcdb/src/entities/possession-indicator/constants/index.ts
@@ -0,0 +1,2 @@
+// 점유율너비 최소 너비
+export const MIN_WIDTH = 30;
---
+++ apps/fcdb/src/entities/possession-indicator/ui/PossessionIndicator.tsx
@@ -0,0 +1,51 @@
+import clsx from "clsx";
+import { MIN_WIDTH } from "../constants";
+
+interface PossessionIndicatorProps {
+  userPossession: number;
+  opponentPossession: number;
+  userNickName: string;
+  opponentNickName: string;
+}
+
+const PossessionIndicator = ({
+  userPossession,
+  opponentPossession,
+  userNickName,
+  opponentNickName,
+}: PossessionIndicatorProps) => {
+  const sectionBaseStyle =
+    "flex items-center justify-between gap-[30px] px-[24px] mobile:px-[16px]";
+  const userSectionStyle = clsx(sectionBaseStyle, "bg-gray-800");
+  const opponentSectionStyle = clsx(sectionBaseStyle, "bg-gray-700");
+
+  const calculatePossessionWidth = (possession: number): string => {
+    return `${Math.max(possession, MIN_WIDTH)}%`;
+  };
+
+  return (
+    <figure className="flex w-full h-[36px] mobile:h-[20px] text-[20px] mobile:text-[16px] font-bold">
+      <section
+        className={userSectionStyle}
+        style={{
+          width: calculatePossessionWidth(userPossession),
+        }}
+      >
+        <span className="truncate whitespace-nowrap">{userNickName}</span>
+        <span className="mobile:hidden">{userPossession}%</span>
+      </section>
+
+      <section
+        className={opponentSectionStyle}
+        style={{
+          width: calculatePossessionWidth(opponentPossession),
+        }}
+      >
+        <span className="mobile:hidden">{opponentPossession}%</span>
+        <span className="truncate whitespace-nowrap">{opponentNickName}</span>
+      </section>
+    </figure>
+  );
+};
+
+export default PossessionIndicator;
