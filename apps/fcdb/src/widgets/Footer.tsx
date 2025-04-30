f3c13d51b133f57c361d8e2ab43b1b38c392279e
jungbyungwook <korjby400@gmail.com>
2025-04-30 09:31:57 +0900

    home-page 반응형, getOuid API 추가 (#14)
    * feat: getOuid API 추가
    
    * feat: home-page mobile 반응형
    
    * feat: footer mobile에서 text size 수정

--- apps/fcdb/src/widgets/Footer.tsx
+++ apps/fcdb/src/widgets/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="w-full bg-gray-900 flex flex-col items-center justify-center py-16">
       <Image src="/logo.svg" alt="logo" width={108} height={20} />
-      <p className="text-[#FFFFFF] text-xl pt-[18px]">
+      <p className="text-[#FFFFFF] text-lg pt-[18px] lg:text-xl">
         Data based on NEXON DEVELOPERS
       </p>
     </footer>
--- apps/fcdb/src/widgets/UserSearchForm.tsx
+++ apps/fcdb/src/widgets/UserSearchForm.tsx
@@ -6,11 +6,11 @@ export const UserSearchForm = () => {
       <input
         name="name"
         placeholder="구단주 이름을 입력해주세요."
-        className="w-[500px] h-14 bg-white text-[#000000] px-4 py-2 rounded-lg"
+        className="w-[64vw] h-10  bg-white text-xs text-[#000000] px-4 py-2  rounded-lg lg:text-[16px] lg:w-[500px] lg:h-14"
       />
       <button
         type="submit"
-        className="ml-4 w-32 h-[60px] bg-gray-900 text-[#FFFFFF] px-4 py-2 rounded-lg hover:bg-gray-800"
+        className="ml-[2vw] w-[24vw] h-10 bg-gray-900 text-xs font-bold text-[#FFFFFF] px-4 py-2 rounded-lg hover:bg-gray-800 lg:text-[16px] lg:ml-4 lg:w-32 lg:h-14"
       >
         전적보기
       </button>
