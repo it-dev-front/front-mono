fb696826c50ffa1533c917f7995ab6a5aef8374f
jungbyungwook <korjby400@gmail.com>
2025-06-05 02:15:17 +0900

    feat: MatchSummary User에 추가

--- apps/fcdb/src/views/User.tsx
+++ apps/fcdb/src/views/User.tsx
@@ -1,15 +1,17 @@
 import { Header } from "@/widgets/Header";
 import { Footer } from "@/widgets/Footer";
 import { UserProfileFetcher } from "@/features/profile/ui/UserProfileFetcher";
+import MatchSummary from "@/widgets/match/MatchSummary";
 
 export const User = async () => {
   return (
     <>
       <Header />
-      <main className="w-full min-w-[366px] flex flex-col min-h-screen pt-[62px]">
-        <div className="w-full flex-grow flex justify-center py-0 md:py-[1rem] border-t border-[#424242] md:border-t-0">
+      <main className="w-full min-w-[366px] flex flex-col min-h-screen pt-[62px] items-center">
+        <div className="w-full flex justify-center py-0 md:py-[1rem] border-t border-[#424242] md:border-t-0">
           <UserProfileFetcher />
         </div>
+        <MatchSummary />
       </main>
       <Footer />
     </>
