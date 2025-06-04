44ad162a9d8d74d203b17f469e04dba838ef406f
jungbyungwook <korjby400@gmail.com>
2025-06-05 03:04:18 +0900

    UserSearchFormation 추가 (#22)
    * feat: MatchSummary User에 추가
    
    * feat: UserSearchFormation 추가

--- apps/fcdb/src/views/User.tsx
+++ apps/fcdb/src/views/User.tsx
@@ -1,14 +1,21 @@
 import { Header } from "@/widgets/Header";
 import { Footer } from "@/widgets/Footer";
 import { UserProfileFetcher } from "@/features/profile/ui/UserProfileFetcher";
+import MatchSummary from "@/widgets/match/MatchSummary";
 
 export const User = async () => {
   return (
     <>
       <Header />
       <main className="w-full min-w-[366px] flex flex-col min-h-screen pt-[62px]">
-        <div className="w-full flex-grow flex justify-center py-0 md:py-[1rem] border-t border-[#424242] md:border-t-0">
+        <div className="w-full flex justify-center py-0 md:py-[1rem] border-t border-[#424242] md:border-t-0">
           <UserProfileFetcher />
+        </div>
+        <div className="w-full flex flex-col items-center gap-[8px]">
+          <MatchSummary />
+          <MatchSummary />
+          <MatchSummary />
+          <MatchSummary />
         </div>
       </main>
       <Footer />
