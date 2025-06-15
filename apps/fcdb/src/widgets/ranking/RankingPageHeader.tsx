2cbc74f265d4ff3e13d2fa13e03d2d5bcf4e9683
JihoonPark <z1@instiz.net>
2025-06-16 00:08:18 +0900

    chore: ranking page header 텍스트 색상 명시

--- apps/fcdb/src/widgets/ranking/RankingPageHeader.tsx
+++ apps/fcdb/src/widgets/ranking/RankingPageHeader.tsx
@@ -1,6 +1,6 @@
 const RankingPageHeader = () => {
   return (
-    <header className="flex justify-center items-center h-[160px] mx-auto text-[28px] w-full">
+    <header className="flex justify-center items-center h-[160px] mx-auto text-[28px] w-full text-white">
       랭킹점수
     </header>
   );
