9ba36a337098a5f64c5204578bba05ff4c730b37
JihoonPark <z1@instiz.net>
2025-06-22 22:50:15 +0900

    chore: 랭킹페이지 background image asset 추가/적용

--- apps/fcdb/src/views/Ranking.tsx
+++ apps/fcdb/src/views/Ranking.tsx
@@ -2,12 +2,20 @@
 
 import RankingPageHeader from "@/widgets/ranking/RankingPageHeader";
 import { RankingTable } from "@/widgets/ranking/RankingTable";
+import Image from "next/image";
 
 export default function Ranking() {
   return (
     <>
-      <main className="w-full min-w-[366px] flex flex-col min-h-screen pt-[62px]">
-        <div className="flex flex-col items-center max-w-[1080px] flex-grow w-full m-auto">
+      <main className="relative w-full min-w-[366px] flex flex-col min-h-screen pt-[62px]">
+        <Image
+          src="/images/ranking-bg.png"
+          width={887}
+          height={485}
+          alt="ranking-bg"
+          className="object-cover fixed bottom-0 left-0 z-0 mobile:hidden"
+        />
+        <div className="flex flex-col items-center max-w-[1080px] flex-grow w-full m-auto z-1">
           <RankingPageHeader />
           <RankingTable />
         </div>
