5d06eb86c81c93f643af0a27d8c73188f72beb00
jungbyungwook <korjby400@gmail.com>
2025-06-23 02:24:13 +0900

    feat: 스쿼드 정보 text, positionIndicator 반응형

--- apps/fcdb/src/features/user-search/ui/UserSearchFormation.tsx
+++ apps/fcdb/src/features/user-search/ui/UserSearchFormation.tsx
@@ -35,19 +35,18 @@ export const UserSearchFormation = ({
 
   return (
     <div>
-      <p className="text-center text-[20px] pt-[16px] pb-[14px]">스쿼드 정보</p>
+      <p className="text-center text-[18px] pt-[20px] pb-[8px] lg:pt-[16px] lg:text-[20px] lg:pb-[14px]">
+        스쿼드 정보
+      </p>
 
-      <div className="flex gap-[24px] px-[20px]">
+      <div className="flex gap-[12px] px-[20px] lg:gap-[24px]">
         <PositionIndicator color="#CE535D" label="FW" />
-
         <PositionIndicator color="#79CD8C" label="MF" />
-
         <PositionIndicator color="#507EED" label="DF" />
-
         <PositionIndicator color="#E67E22" label="GK" />
       </div>
 
-      <div className="pt-[12px] px-[16px]">
+      <div className="pt-[10px] px-[8px] lg:pt-[12px] lg:px-[16px]">
         <div
           className="relative w-full h-0 pb-[60%] bg-contain bg-center bg-no-repeat"
           style={{
