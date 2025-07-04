b80debc8a99b66f77918048a01c574eb63c39bb5
byeongwook <jby@instiz.net>
2025-07-04 12:32:38 +0900

    feat: BallSpinner 이미지 수정 및 적용

--- apps/fcdb/src/features/match/ui/MatchSummaryFetcher.tsx
+++ apps/fcdb/src/features/match/ui/MatchSummaryFetcher.tsx
@@ -4,11 +4,17 @@ import MatchSummary from "@/widgets/match/MatchSummary";
 import { Fragment } from "react";
 import { useMatchFetcher } from "@/entities/match/providers/MatchProvider";
 import { MatchSummaryType } from "@/entities/match/types/match.info.types";
+import { BallSpinner } from "@/shared/ui/spinner/BallSpinner";
 
 export const MatchSummaryFetcher = () => {
   const { matches, isMatchesLoading } = useMatchFetcher();
 
-  if (isMatchesLoading) return <div>...loading</div>;
+  if (isMatchesLoading)
+    return (
+      <div className="flex justify-center items-center min-h-screen w-full">
+        <BallSpinner />
+      </div>
+    );
   if (!matches) return null;
 
   return (
--- apps/fcdb/src/shared/ui/spinner/BallSpinner.tsx
+++ apps/fcdb/src/shared/ui/spinner/BallSpinner.tsx
@@ -1,55 +1,14 @@
 import Image from "next/image";
 
-interface BallSpinnerProps {
-  size?: number;
-  ballSize?: number;
-  gap?: number; // 공 사이 간격
-}
-
-const BALL_COUNT = 8;
-const BALLS = Array.from({ length: BALL_COUNT });
-
-export const BallSpinner = ({
-  size = 20,
-  ballSize = 26,
-  gap = 90,
-}: BallSpinnerProps) => {
-  // gap만큼 반지름을 더 줄여 공 사이에 여유를 둠
-  const radius = size / 2 - ballSize / 2 - gap;
-
+export const BallSpinner = () => {
   return (
-    <div
-      className="absolute animate-spin"
-      style={{ width: size, height: size }}
-    >
-      {BALLS.map((_, i) => {
-        const angle = (i * 360) / BALL_COUNT;
-        const rad = (angle * Math.PI) / 180;
-        const x = size / 2 + radius * Math.cos(rad);
-        const y = size / 2 + radius * Math.sin(rad);
-        const opacity = 1 - 0.7 * (i / BALL_COUNT);
-
-        return (
-          <div
-            key={i}
-            className="absolute"
-            style={{
-              left: x,
-              top: y,
-              transform: `translate(-50%, -50%)`,
-              opacity,
-            }}
-          >
-            <Image
-              src="/images/ball.png"
-              alt="spinner ball"
-              width={ballSize}
-              height={ballSize}
-              style={{ width: ballSize, height: ballSize }}
-            />
-          </div>
-        );
-      })}
+    <div>
+      <Image
+        src="/images/ball-spinner.gif"
+        width={60}
+        height={60}
+        alt="ball-spinner"
+      ></Image>
     </div>
   );
 };
