091c87c94599a3576b26d7ff40f81f3bad24a0a5
dahoon06 <dahoon226@naver.com>
2025-07-13 00:26:40 +0900

    feat: 메타 태그 추가

--- apps/fcdb/src/app/(without-footer)/user/[name]/page.tsx
+++ apps/fcdb/src/app/(without-footer)/user/[name]/page.tsx
@@ -1,7 +1,19 @@
 import { User } from "@/views/User";
+import { Metadata } from "next";
 
 interface UserPageProps {
-  params: Promise<{ name: string }>;
+  params: { name: string };
+}
+
+export async function generateMetadata({
+  params,
+}: UserPageProps): Promise<Metadata> {
+  const { name } = await params;
+  const decodedName = decodeURIComponent(name);
+  return {
+    title: `${decodedName} | FCDB - FC 온라인 전적 검색`,
+    description: `${decodedName}님의 FC 온라인 전적 정보입니다.`,
+  };
 }
 
 export default async function UserPage({ params }: UserPageProps) {
